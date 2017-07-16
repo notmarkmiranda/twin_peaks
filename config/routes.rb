868c135d226101dbf256615ba17ca8b79a5266fa
Mark Miranda <notmarkmiranda@gmail.com>
2017-07-16 08:55:10 -0600

    might have gone too far, thinking about going backwards

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   root 'pages#index'
 
   resources :users, only: [:show, :new, :create]
+  resources :roles
   get '/login', to: 'sessions#new', as: 'login'
   post '/login', to: 'sessions#create'
   get '/logout', to: 'sessions#destroy', as: 'logout'
