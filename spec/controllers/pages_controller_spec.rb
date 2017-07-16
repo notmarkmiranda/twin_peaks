6fd54bf3a561e7c1f5273ad30e1b0a612846c392
Mark Miranda <notmarkmiranda@gmail.com>
2017-07-16 09:56:57 -0600

    update page controller testing for not found

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -1,10 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe PagesController, type: :controller do
-  context "GET#index" do
-    it "renders the index template" do
+  context 'GET#index' do
+    it 'renders the index template' do
       get :index
       expect(response).to render_template :index
+    end
+  end
+
+  context 'GET#not_found' do
+    it 'renders the not found template' do
+      get :not_found
+      expect(response).to render_template :not_found
     end
   end
 end
