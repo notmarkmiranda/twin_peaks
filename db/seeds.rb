4d139b5dcbebdcb1e96514bff715e5d608aba4de
Mark Miranda <notmarkmiranda@gmail.com>
2017-07-18 09:22:45 -0600

    initial branch commit

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,14 +1,3 @@
-admin = Role.create!(title: 'Admin')
 u = User.create!(login: '07141206', password: 'password')
-u.roles << admin
+u.roles = [:admin]
 puts "#{u.login} created!"
-
-
-# testing identifiers, remove before submitting to prod
-pi = Role.create!(title: 'Primary Investigator')
-cc = Role.create!(title: 'Coordinator')
-pa = Role.create!(title: 'Patient')
-
-User.create!(login: '123456', password: 'password').roles << pi
-User.create!(login: '654321', password: 'password').roles << cc
-User.create!(login: '123123', password: 'password').roles << pa
