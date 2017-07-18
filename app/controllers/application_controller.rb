65850fd950f67c59525b639c3951af00ddbe867a
Mark Miranda <notmarkmiranda@gmail.com>
2017-07-17 21:23:29 -0600

    closes #40 #41

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,11 +2,16 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   helper_method :current_user
   helper_method :require_admin
+  helper_method :require_user
 
   private
 
   def current_user
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  end
+
+  def require_user
+    redirect_to not_found_path unless current_user
   end
 
   def require_admin
