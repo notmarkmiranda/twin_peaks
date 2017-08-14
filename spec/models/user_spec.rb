0b4cd75681e4d0cea28e95db7a21069abf3dc695
Mark Miranda <notmarkmiranda@gmail.com>
2017-08-14 11:22:06 -0600

    Add study database and model
    initial branch commit
    
    closes #49

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -5,4 +5,8 @@ RSpec.describe User do
     it { should validate_presence_of :login }
     it { should validate_uniqueness_of :login }
   end
+
+  context 'relationships' do
+    it { should have_many :studies }
+  end
 end
